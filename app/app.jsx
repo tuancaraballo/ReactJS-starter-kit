c9b2cb227c29b560212da5671882bd524f601131
Antuan Caraballo <tranc2606@gmail.com>
2017-07-05 23:32:37 -0700

    deleted old components

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var Greeter = require('Greeter');   --> Example of import
+
+ReactDOM.render(
+  // <Greeter name={country} sms={msg}/>,
+  <h1> Starter Kit app </h1>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,6 @@
-//Description
-
-/*
-	Set entry point that webpack will look for
-	Since there is jsx code in that file (app.jsx), webpack doesn't know what to do by
-	default, so this is why we use a babel loader, and babel by itself requires presets
-	that are react and es2015. These presets tell babel what to do
-	Then we specify which files we want the loader to be applied to tests and we also
-	specify the files we want to exclude
-
-	After writing this file you must run the webpack command (webpack)
-
-	Now, you can start importing modules into the app.jsx and now you can remove
-	any react dependency in the index.html file
-*/
-
-// This exports an object that tells webpack what it should do
 module.exports = {
 	// where it should start processing your code
-	entry: './public/app.jsx',
+	entry: './app/app.jsx',
 	// specify the output
 	output: {
 		path: __dirname, //-> specify the directory root
@@ -29,9 +12,7 @@ module.exports = {
 	//--> this let us import components by their name without having
 	  //  to specify their paths. ex:  require('Greeter') vs require(./components/Greeter)
 		alias: {
-			Greeter: __dirname + '/public/components/Greeter.jsx',
-			GreeterMessage: __dirname + '/public/components/GreeterMessage.jsx',
-			GreeterForm: __dirname + '/public/components/GreeterForm.jsx'
+			// Greeter: __dirname + '/public/components/Greeter.jsx',
 		},
 		
 	},
